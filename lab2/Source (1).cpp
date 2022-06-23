197ff6890984534be073882d59b32075766c9300
Onton <93614001+Ontonchik@users.noreply.github.com>
2022-06-23 14:06:14 +0300

    Update Source (1).cpp

--- lab2/Source (1).cpp
+++ lab2/Source (1).cpp
@@ -2,23 +2,20 @@
 #include <conio.h>
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "RUS");
-	wstring child_name(L"C:/Users/sveta/Desktop/c++/lab 2 oc/Debug/Child.exe");
-	LPWSTR name = &child_name[0];
-	string line = "";
-	cout << "Введите размер массива: " << endl;
+	std::string line = "Child.exe ";
+	std::cout << "Р’РІРµРґРёС‚Рµ СЂР°Р·РјРµСЂ РјР°СЃСЃРёРІР°: " << std::endl;
 	int length;
-	cin >> length;
+	std::cin >> length;
 	int* array = new int[length];
-	line += to_string(length);
+	line += std::to_string(length);
 	for (int i = 0; i < length; i++) {
-		cin >> array[i];
-		line += " "+to_string(array[i]);
+		std::cin >> array[i];
+		line += " "+ std::to_string(array[i]);
 	}
-	wstring c(line.begin(), line.end());
+	std::wstring c(line.begin(), line.end());
 	LPWSTR command_line = &c[0];
 	STARTUPINFO si;
 	PROCESS_INFORMATION piApp;
@@ -26,18 +23,32 @@ int main() {
 	ZeroMemory(&si, sizeof(STARTUPINFO));
 	si.cb = sizeof(STARTUPINFO);
 	si.lpTitle = child_;
-	boolean check=CreateProcess(name, command_line, NULL, NULL, FALSE,
+	boolean check=CreateProcess(NULL, command_line, NULL, NULL, FALSE,
 		CREATE_NEW_CONSOLE, NULL, NULL, &si, &piApp);
 	if (!check){
 		_cputs("process not created");
 		return 0;
 	}
 	_cputs("The new process is created.\n");
-	// ждем завершения созданного прцессаCloseHandle(piF.hThread);
+	// Р¶РґРµРј Р·Р°РІРµСЂС€РµРЅРёСЏ СЃРѕР·РґР°РЅРЅРѕРіРѕ РїСЂС†РµСЃСЃР°CloseHandle(piF.hThread);
 	WaitForSingleObject(piApp.hProcess, INFINITE);
-	// закрываем дескрипторы этого процесса в текущем процессе
+	// Р·Р°РєСЂС‹РІР°РµРј РґРµСЃРєСЂРёРїС‚РѕСЂС‹ СЌС‚РѕРіРѕ РїСЂРѕС†РµСЃСЃР° РІ С‚РµРєСѓС‰РµРј РїСЂРѕС†РµСЃСЃРµ
+	CloseHandle(piApp.hThread);
+	CloseHandle(piApp.hProcess);
+
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+	line = "Count.exe";
+	std::wstring str(line.begin(), line.end());
+	command_line = &str[0];
+	boolean check2=CreateProcess(NULL, command_line, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &piApp);
+	if (check2) {
+		std::cout << "New process has created";
+	}
+	SetPriorityClass(piApp.hProcess, HIGH_PRIORITY_CLASS);
+	Sleep(3000);
+	TerminateProcess(piApp.hProcess, 0);
 	CloseHandle(piApp.hThread);
 	CloseHandle(piApp.hProcess);
 	ExitProcess(0);
-	return 0;
 }
